77bca20050cfe735f564c4237ef34a94623b6dd9
danst3in <50937807+danst3in@users.noreply.github.com>
2020-12-16 17:24:56 -0800

    Create counter app and add to index

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,11 @@
 /* Add JavaScript code here! */
 import React from 'react'
 import ReactDOM from 'react-dom'
-ReactDOM.render(<div>"Hello REACT"</div>, document.getElementById('root'));
+import App from './App';
+// ReactDOM.render(<div>"Hello REACT"</div>, document.getElementById('root'));
 // console.log('Hello World! You did! Welcome to Snowpack :D');
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'));
