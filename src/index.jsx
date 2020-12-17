5524eaeb1f57d640e31a89ead8862ebdc746a667
danst3in <50937807+danst3in@users.noreply.github.com>
2020-12-16 17:16:25 -0800

    Create folder structure

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+/* Add JavaScript code here! */
+import React from 'react'
+import ReactDOM from 'react-dom'
+ReactDOM.render(<div>"Hello REACT"</div>, document.getElementById('root'));
+// console.log('Hello World! You did! Welcome to Snowpack :D');
